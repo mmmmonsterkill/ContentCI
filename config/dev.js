b3ecdb14abe50806d050d354c794df40918fe979
mmmmonsterkill <test_rzr_oedf_6189@hotmail.com>
2022-12-06 13:52:08 +0000

    Travis config

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,7 @@
 module.exports = {
-  googleClientID:
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
+  googleClientID: '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://pichpoch:CSB0QSXAJ5k7QKuX@ac-fijmk8n-shard-00-00.xnjcekj.mongodb.net:27017,ac-fijmk8n-shard-00-01.xnjcekj.mongodb.net:27017,ac-fijmk8n-shard-00-02.xnjcekj.mongodb.net:27017/blog-dev?ssl=true&replicaSet=atlas-5svt00-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
 };
---
+++ tests/setup.js
@@ -0,0 +1,10 @@
+// uncomment if test are taking too much time
+// jest.setTimeout(30000)
+
+require('../models/User')
+
+const mongoose = require('mongoose')
+const keys = require('../config/keys')
+
+mongoose.Promise = global.Promise
+mongoose.connect(keys.mongoURI, { useMongoClient: true })
